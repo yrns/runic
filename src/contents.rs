fc3836bb166e20a9e16ffce0366619f79c2ecddc
Al McElrath <hello@yrns.org>
2024-07-29 18:53:27 -0700

    Header module.

--- src/contents.rs
+++ src/contents.rs
@@ -1,13 +1,15 @@
+pub mod expanding;
+pub mod grid;
+pub mod header;
+pub mod inline;
+pub mod section;
+
 use crate::*;
 pub use expanding::*;
 pub use grid::*;
+pub use header::*;
 pub use inline::*;
 pub use section::*;
-
-pub mod expanding;
-pub mod grid;
-pub mod inline;
-pub mod section;
 
 /// A widget to display the contents of a container.
 pub trait Contents {
---
+++ src/contents/header.rs
@@ -0,0 +1,60 @@
+use crate::*;
+
+#[derive(Clone, Debug)]
+pub struct HeaderContents<T> {
+    // Box<dyn Fn(...)> ? Not clonable.
+    pub header: String,
+    pub contents: T,
+}
+
+impl<T> HeaderContents<T> {
+    pub fn new(header: impl Into<String>, contents: T) -> Self {
+        Self {
+            header: header.into(),
+            contents,
+        }
+    }
+}
+
+pub type ContentsStorage = HashMap<usize, (Box<dyn Contents>, Vec<(usize, Item)>)>;
+
+impl<T> Contents for HeaderContents<T>
+where
+    T: Contents,
+{
+    fn len(&self) -> usize {
+        self.contents.len()
+    }
+
+    fn pos(&self, slot: usize) -> egui::Vec2 {
+        self.contents.pos(slot)
+    }
+
+    fn slot(&self, offset: egui::Vec2) -> usize {
+        self.contents.slot(offset)
+    }
+
+    fn accepts(&self, item: &Item) -> bool {
+        self.contents.accepts(item)
+    }
+
+    fn fits(&self, ctx: Context, egui_ctx: &egui::Context, item: &DragItem, slot: usize) -> bool {
+        self.contents.fits(ctx, egui_ctx, item, slot)
+    }
+
+    fn ui(
+        &self,
+        ctx: Context,
+        q: &ContentsStorage,
+        drag_item: &Option<DragItem>,
+        items: &[(usize, Item)],
+        ui: &mut egui::Ui,
+    ) -> InnerResponse<MoveData> {
+        // Is InnerResponse really useful?
+        let InnerResponse { inner, response } = ui.vertical(|ui| {
+            ui.label(&self.header);
+            self.contents.ui(ctx, q, drag_item, items, ui)
+        });
+        InnerResponse::new(inner.inner, response)
+    }
+}
